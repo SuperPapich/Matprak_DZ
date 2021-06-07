97d8f31a106081ca566a92ba982923df8595b55e
EverlaststingRolich <60112075+EverlaststingRolich@users.noreply.github.com>
2021-06-07 12:57:26 +0300

    dop_clear_data

--- lab4.py
+++ lab4.py
@@ -33,7 +33,7 @@ def get_data():
     return data
 
 
-def easy_stat(data) -> None:
+def easy_stat(data):
     """ №2 Простая статистика"""
     # Количество элементов
     count_data = data.count()
@@ -60,7 +60,76 @@ def hard_stat(date):
     return correlation
 
 
+def clear_data(data):
+    """ №4 Очистка данных"""
+    # Дропамем колонку зипкод
+    data.drop("zipcode", axis=1, inplace=True)
+    data.set_index("id", inplace=True)
+    # print(data.head(10))
+    # print(data.shape)
+
+
+def basic_work_with_date(data):
+    """ №5 Базовая обработка дат"""
+    # Конвертируем дату в удобный формат
+    data.date = pd.to_datetime(data.date)
+    # Вытаскиваем из даты нужные части
+    data["year"] = data.date.dt.year
+    data["month"] = data.date.dt.month
+    data["day"] = data.date.dt.day
+    data["weekday"] = data.date.dt.weekday
+    # Дропаем изначальну колонку даты
+    data.drop("date", axis=1, inplace=True)
+
+    # print(data.head(10))
+    # print(data.shape)
+
+
+def build_histogram(data):
+    """ №6 Построение гистограммы"""
+    # Строим гистограму
+    plt.hist(data.price, bins=100)
+    plt.title("Price distribution")
+    plt.xlabel("Price")
+    plt.ylabel("NUmber of flats")
+    # plt.show()
+
+
+def build_poin_diagram(correlation):
+    """ №7 Построение точечной диаграммы """
+    # print(correlation.price.sort_values())
+    x = correlation.price
+    y = correlation.sqft_living
+    colors = np.random.rand(20)
+    plt.rcParams["axes.facecolor"] = "#CBF5EF"
+    plt.scatter(x, y, c=colors, alpha=1, marker="1")
+    plt.title("Price correlation")
+    plt.xlabel("Price")
+    plt.ylabel("Sqft_living")
+    #plt.show()
+
+
+def dop_clear_data(data):
+    """ №8 Дополнительная очистка данных """
+    data.price.quantile(0.99)
+    data = data[data.price < data.price.quantile(0.99)]
+    # print(data.shape)
+
+
+def more_graphics(data,correlation):
+    """ №9 Построить доп. графики """
+    pass
+
+
+
+
 if __name__ == "__main__":
     data = get_data()
     easy_stat(data)
     correlation = hard_stat(data)
+    clear_data(data)
+    basic_work_with_date(data)
+    #build_histogram(data)
+    #build_poin_diagram(correlation)
+    dop_clear_data(data)
+    more_graphics(data, correlation)
